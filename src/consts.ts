76b69a78389dd1e77e7011cde7290798877968a5
shrijee-consultants <ankit.saraf.07@gmail.com>
2025-08-16 23:15:34 +0530

    first changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "ShriJee Consultants";
+export const SITE_DESCRIPTION = "Welcome to ShriJee Consultants run by Ankit Saraf COA Certified Architect!";
